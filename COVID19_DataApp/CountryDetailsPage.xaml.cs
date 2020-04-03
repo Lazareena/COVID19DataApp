9ad5b7debca6aa6d30f7272f5ba26f84beadce73
Lazareena Thaveethu <reena@Lazareenas-MBP.lan>
2020-04-03 16:40:12 +0800

    Added CountryDetailsPage

---
+++ COVID19_DataApp/CountryDetailsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using DataLayer.Models;
+using Xamarin.Forms;
+
+namespace COVID19_DataApp
+{
+    public partial class CountryDetailsPage : Frame
+    {
+        private CountryData _countryData;
+        public Action OnClose { get; set; }
+
+        public CountryDetailsPage(CountryData countryData)
+        {
+            _countryData = countryData;
+            InitializeComponent();
+            this.BindingContext = countryData;
+        }
+
+        private void ButtonClose_Clicked(System.Object sender, System.EventArgs e)
+        {
+            OnClose?.Invoke();
+        }
+    }
+}
--- COVID19_DataApp/MainPage.xaml.cs
+++ COVID19_DataApp/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DataLayer;
 using DataLayer.Models;
@@ -27,7 +29,29 @@ namespace COVID19_DataApp
         //TODO: Setup UI for displaying COVID 19 data by country
         public void InitializeUI()
         {
+            var displayCovidCountryList = _covidCountryData;
+            CountryList.ItemsSource = displayCovidCountryList;
 
+            SearchEntry.TextChanged += SearchEntry_TextChanged;
+        }
+
+        SemaphoreSlim _searchEntryLock = new SemaphoreSlim(1);
+        private void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!_searchEntryLock.Wait(0))
+                return;
+
+            var searchTerm = SearchEntry.Text.ToLower();
+
+            List<CountryData> updatedList = null;
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                updatedList = _covidCountryData;
+            else
+                updatedList = _covidCountryData.Where(c => c.CountryName.Length >= searchTerm.Length &&
+                                                           (string.Equals(c.CountryName, searchTerm, StringComparison.InvariantCultureIgnoreCase)
+                                                            || c.CountryName.ToLower().Contains(searchTerm))).ToList();
+            CountryList.ItemsSource = updatedList;
+            _searchEntryLock.Release();
         }
 
         protected async override void OnAppearing()
@@ -41,9 +65,25 @@ namespace COVID19_DataApp
             else
             {
                 _covidCountryData = covidDataResult.Result;
-                errMsg.Text = "Successfully loaded data from https://www.worldometers.info/coronavirus";
                 InitializeUI();
             }
+        }
+
+        private void Country_Tapped(System.Object sender, System.EventArgs e)
+        {
+            var frame = sender as Frame;
+            var bindingContext = frame?.BindingContext as CountryData;
+
+            if (bindingContext == null)
+                return;
+
+            var countryDetailsPage = new CountryDetailsPage(bindingContext);
+
+            countryDetailsPage.OnClose = () =>
+            {
+                MainLayout.Children.Remove(countryDetailsPage);
+            };
+            MainLayout.Children.Add(countryDetailsPage);
         }
     }
 }
